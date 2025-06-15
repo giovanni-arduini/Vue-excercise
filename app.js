9ec2decc414c2783b16d23b67f7feb46167305e2
giovanni-arduini <gionnibigudd@gmail.com>
2025-06-15 19:53:57 +0200

    ADD first Vue js file, data & methods

---
+++ app.js
@@ -0,0 +1,20 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      message: "Learn Vue.js!",
+      vueLink: "https://vuejs.org/",
+    };
+  },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return "Learn Vue!";
+      } else {
+        return "Master Vue!";
+      }
+    },
+  },
+});
+
+app.mount("#user-goal");
