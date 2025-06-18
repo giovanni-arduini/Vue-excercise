c8edf47b5344c4b988ab9b02467c775d963bf1b4
giovanni-arduini <gionnibigudd@gmail.com>
2025-06-18 11:36:34 +0200

    ADD

--- Exercises/2 - Events/app.js
+++ Exercises/2 - Events/app.js
@@ -2,11 +2,18 @@ const app = Vue.createApp({
   data() {
     return {
       name: "",
+      confirmedName: "",
     };
   },
   methods: {
     showAlert() {
       alert("Button pressed!");
+    },
+    setName(event) {
+      this.name = event.target.value;
+    },
+    confirmName() {
+      this.confirmedName = this.name;
     },
   },
 });
