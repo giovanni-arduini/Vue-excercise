1a795f6bbd9fdd85583e2865791ff58bf47b3e0c
giovanni-arduini <gionnibigudd@gmail.com>
2025-06-23 15:43:51 +0200

    Add computed styles & special attack & heal

--- Monster Slayer/app.js
+++ Monster Slayer/app.js
@@ -1,16 +1,29 @@
 function getRandomValue(min, max) {
-  return Math.floor(random() * (max - min)) + min;
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 const app = Vue.createApp({
   data() {
     return {
       playerHealth: 100,
-      monsterHealth: 120,
+      monsterHealth: 100,
+      currentRound: 0,
     };
+  },
+  computed: {
+    playerBarStyles() {
+      return { width: this.playerHealth + "%" };
+    },
+    monsterBarStyles() {
+      return { width: this.monsterHealth + "%" };
+    },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0 || this.currentRound === 0;
+    },
   },
   methods: {
     attackMonster() {
+      this.currentRound++;
       const attack = getRandomValue(5, 12);
       this.monsterHealth -= attack;
       this.attackPlayer();
@@ -19,7 +32,23 @@ const app = Vue.createApp({
       const attack = getRandomValue(7, 15);
       this.playerHealth -= attack;
     },
+    specialAttackMonster() {
+      this.currentRound++;
+      const attack = getRandomValue(11, 25);
+      this.monsterHealth -= attack;
+      this.attackPlayer();
+    },
+    healPlayer() {
+      currentRound++;
+      const healValue = getRandomValue(8, 14);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healValue;
+      }
+      this.attackPlayer();
+    },
   },
 });
 
-app.mount("game");
+app.mount("#game");
