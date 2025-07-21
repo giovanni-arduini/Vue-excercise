31c39359a1b174c26ea3b39f167fcd128da96220
giovanni-arduini <gionnibigudd@gmail.com>
2025-07-21 12:36:31 +0200

    ADD vue app and data

---
+++ Components/app.js
@@ -0,0 +1,20 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "0123 3456",
+          email: "ml@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "0123 3456",
+          email: "jj@localhost.com",
+        },
+      ],
+    };
+  },
+});
